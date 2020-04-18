ba9bef99bdd4a5f51e02a71a62af8bd620f4db6f
Himanshu raj <himanshu@foodmonk.com>
2020-04-18 18:31:53 +0530

    fixes

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ res.sendFile(path.join(__dirname = 'client/build/index.html'));
   });
 }
 app.get('*',(req,res) =>{
-  res.sendFile(path.join(__dirname = 'client/build/index.html'));
+  res.sendFile(path.join(__dirname = '/client/build/index.html'));
 })
 
 module.exports = app;
